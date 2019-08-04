49e1f9580a969667172f2b33083a71acc231d12c
Dung Ta Van <itprono3@gmail.com>
2019-08-04 13:24:51 +0700

    update clients and remove main.cpp

--- src/EzyClients.cpp
+++ src/EzyClients.cpp
@@ -10,41 +10,63 @@ EzyClients::EzyClients() {
 }
 
 EzyClient* EzyClients::newClient(config::EzyClientConfig* config) {
-    auto client = new EzyClient(config);
-    if(mDefaultClientName.length() == 0)
-        mDefaultClientName = client->getName();
-    addClient(client);
+    std::unique_lock<std::mutex> lock(mMutex);
+    return newClient0(config);
+}
+
+EzyClient* EzyClients::newClient0(config::EzyClientConfig* config) {
+    auto clientName = config->getClientName();
+    auto client = mClients[clientName];
+    if(!client) {
+        client = new EzyClient(config);
+        addClient0(client);
+        if(mDefaultClientName.length() == 0)
+            mDefaultClientName = client->getName();
+    }
     return client;
 }
 
 EzyClient* EzyClients::newDefaultClient(config::EzyClientConfig* config) {
-    auto client = newClient(config);
+    std::unique_lock<std::mutex> lock(mMutex);
+    auto client = newClient0(config);
     mDefaultClientName = client->getName();
     return client;
 }
 
 void EzyClients::addClient(EzyClient* client) {
     std::unique_lock<std::mutex> lock(mMutex);
+    addClient0(client);
+}
+
+void EzyClients::addClient0(EzyClient* client) {
     mClients[client->getName()] = client;
 }
 
 EzyClient* EzyClients::getClient(std::string name) {
+    std::unique_lock<std::mutex> lock(mMutex);
+    return getClient0(name);
+}
+
+EzyClient* EzyClients::getClient0(std::string name) {
     auto answer = mClients[name];
     return answer;
 }
 
 EzyClient* EzyClients::getDefaultClient() {
-    auto client = getClient(mDefaultClientName);
+    std::unique_lock<std::mutex> lock(mMutex);
+    if(mDefaultClientName.length() == 0)
+        return NULL;
+    auto client = getClient0(mDefaultClientName);
     return client;
 }
 
-void EzyClients::getClients(std::vector<EzyClient*>& buffer) {
-    buffer.clear();
+void EzyClients::getClients(std::vector<EzyClient*>& cachedClients) {
+    cachedClients.clear();
     std::unique_lock<std::mutex> lock(mMutex);
     EZY_FOREACH_MAP(mClients) {
         auto client = it->second;
         if(client)
-            buffer.push_back(it->second);
+            cachedClients.push_back(it->second);
     }
 }
 
